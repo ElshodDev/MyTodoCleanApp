a7f3acb9fe262bc5985a89ef635e5e6cfca3dcbc
Elshod Ibodullayev <elshod@example.com>
2026-01-30 23:39:52 +0500

    feat: complete full CRUD for TodoItems and Users

---
+++ MyTodoCleanApp.Application/TodoItems/Commands/UpdateTodoItem/UpdateTodoItemCommand.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MyTodoCleanApp.Application.Common.Interfaces;
+
+namespace MyTodoCleanApp.Application.TodoItems.Commands.UpdateTodoItem;
+
+public record UpdateTodoItemCommand(Guid Id, string Title, string Note, bool IsDone) : IRequest;
+
+public class UpdateTodoItemCommandHandler : IRequestHandler<UpdateTodoItemCommand>
+{
+    private readonly IApplicationDbContext _context;
+    public UpdateTodoItemCommandHandler(IApplicationDbContext context) => _context = context;
+
+    public async Task Handle(UpdateTodoItemCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.TodoItems.FindAsync(new object[] { request.Id }, cancellationToken);
+
+        if (entity == null)
+        {
+            throw new Exception("Vazifa topilmadi!");
+        }
+
+        entity.Title = request.Title;
+        entity.Note = request.Note;
+        entity.IsDone = request.IsDone;
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
